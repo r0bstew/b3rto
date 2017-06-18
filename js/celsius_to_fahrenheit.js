75fa2887a1ed751b44d042ff3d7b3f2e1ff4e136
Rob Stewart <rlxstew@gmail.com>
2017-06-18 13:27:40 -0400

    script to convert celsius to fahrenheit

---
+++ js/celsius_to_fahrenheit.js
@@ -0,0 +1,14 @@
+//Convert Celsius to Fahrenheit
+
+
+function convertToF(celsius) {
+  var fahrenheit;
+  // Only change code below this line
+  fahrenheit = celsius*9/5+32 + "F";
+  
+  // Only change code above this line
+  return fahrenheit;
+}
+
+// Change the inputs below to test your code
+convertToF(30);
