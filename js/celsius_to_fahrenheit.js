d5914acf0f7827b0b0ff2f8a866c1d25d77a5fb9
r0bstew <rlxstew@gmail.com>
2017-06-28 16:16:34 -0400

    Update celsius_to_fahrenheit.js

--- js/celsius_to_fahrenheit.js
+++ js/celsius_to_fahrenheit.js
@@ -3,12 +3,11 @@
 
 function convertToF(celsius) {
   var fahrenheit;
-  // Only change code below this line
+
   fahrenheit = celsius*9/5+32 + "F";
   
-  // Only change code above this line
   return fahrenheit;
 }
 
-// Change the inputs below to test your code
+// Change the inputs below to test the code
 convertToF(30);
