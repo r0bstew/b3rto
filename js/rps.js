213d4b8f9a271746cbc6ad0489bf9556ad08eaea
Robert Stewart <robert.stewart@leonardo.com>
2017-07-24 12:36:09 -0400

    adding rock-paper-scissors game

---
+++ js/rps.js
@@ -0,0 +1,109 @@
+var buttonRock = document.getElementById("action-rock");
+var buttonPaper = document.getElementById("action-paper");
+var buttonScissors = document.getElementById("action-scissors");
+
+//buttonRock.innerHTML="interesting..";
+//buttonRock.style.backgroundColor="red";
+
+buttonRock.addEventListener("click", function() {
+	play("rock");
+});
+
+buttonPaper.addEventListener("click", function() {
+	play("paper");
+});
+
+buttonScissors.addEventListener("click", function() {
+	play("scissors");
+});
+
+function play(playerSelection) {
+	//alert("You played " + playerSelection);
+  var computerSelection = getComputerRandomSelection();
+	var matchResult = determineMatchResult(playerSelection, computerSelection);
+  var playerScore = parseInt(document.getElementById("player-score").innerHTML);	
+  var computerScore = parseInt(document.getElementById("computer-score").innerHTML);
+  var message;
+  
+  if (matchResult === 0) {
+  	message = "IT'S A TIE!";
+  }	else if (matchResult === 1) {
+  	message = "YOU WON!";
+    playerScore += 1;
+  } else if (matchResult === 2) {
+  	message = "COMPUTER WINS!";
+    computerScore += 1;
+  }
+  
+  displayResults(playerSelection, computerSelection, message, playerScore, computerScore);
+  determineGameResult(playerScore, computerScore); 
+}
+
+function getComputerRandomSelection() {
+
+	var options = ["rock","paper","scissors"];
+  return options[Math.floor(Math.random() * options.length)];
+  
+}
+
+function determineMatchResult(playerSelection, computerSelection) {
+
+	if (playerSelection === computerSelection) {
+		return 0;
+	} else {
+  	if (playerSelection === "paper") {
+  		if (computerSelection === "rock") {
+  			return 1;
+  		}
+      if (computerSelection === "scissors") {
+      	return 2;
+        }
+        
+      } else if (playerSelection === "rock") {
+      	if (computerSelection === "paper") {
+        	return 2;
+        }
+       	if (computerSelection === "scissors") {
+        	return 1;
+        	}
+        } else if (playerSelection === "scissors") {
+        	if (computerSelection === "paper") {
+          	return 1;
+					}
+          if (computerSelection === "rock") {
+          return 2;
+          }
+        }
+      }
+    }
+
+function determineGameResult(playerScore, computerScore) {
+
+	if(playerScore === 5) {
+		document.getElementById("message").innerHTML = "GAME OVER - YOU WON!";
+    resetGame();
+    } else if (computerScore === 5) {
+    document.getElementById("message").innerHTML = "GAME OVER - COMPUTER WINS!";
+    resetGame();
+    }
+
+}
+
+function resetGame() {
+
+	document.getElementById("player-said").innerHTML = "";
+  document.getElementById("computer-said").innerHTML = "";
+  document.getElementById("player-score").innerHTML = "0";
+  document.getElementById("computer-score").innerHTML = "0";
+
+}
+
+function displayResults(playerSaid, computerSaid, message, playerScore, computerScore) {
+
+	document.getElementById("player-said").innerHTML = playerSaid;
+  document.getElementById("computer-said").innerHTML = computerSaid;
+  document.getElementById("message").innerHTML = message;
+  document.getElementById("player-score").innerHTML = playerScore;
+  document.getElementById("computer-score").innerHTML = computerScore;
+  
+}
